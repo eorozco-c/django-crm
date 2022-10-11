a1c4815eab0dd4cb44fb9cf689693636c08a26a0
Ernesto Orozco C <ernestorozco94@gmail.com>
2022-10-11 17:52:03 -0300

    make it better

--- apps/requerimientos/admin.py
+++ apps/requerimientos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Requerimiento, TipoRequerimiento
 
 # Register your models here.
+
+admin.site.register(Requerimiento)
+admin.site.register(TipoRequerimiento)
