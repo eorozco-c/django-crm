f6fbe0483c2c87a42609079f0664cd9d9f81d542
Ernesto Orozco C <ernestorozco94@gmail.com>
2022-10-14 03:26:32 -0300

    com

--- apps/articulos/models.py
+++ apps/articulos/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from apps.empresas.models import Empresa
 
 # Create your models here.
+class Estado(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
 class Articulo(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField(blank=True, null=True)
     stock = models.IntegerField()
     ubicacion = models.CharField(max_length=255, blank=True, null=True)
     estado = models.BooleanField(default=True)
-    articulo_estado = (
-        ('1', 'EXTRAVIADO'),
-        ('2', 'PERDIDO'),
-        ('3', 'NUEVO'),
-        ('4', 'USADO'),
-        ('5', 'OBSOLETO'),
-    )
+    articulo_estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name="articulos_empresa")
